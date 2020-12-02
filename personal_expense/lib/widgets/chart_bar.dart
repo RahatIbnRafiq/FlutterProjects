6ac030f9b44b614aeb6308b608aec0401bbb6205
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-02 18:37:54 -0500

    adding layout builder

--- personal_expense/lib/widgets/chart_bar.dart
+++ personal_expense/lib/widgets/chart_bar.dart
@@ -9,46 +9,50 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            height: 20,
-            child: FittedBox(
-                child: Text('${this.spendingAmount.toStringAsFixed(0)}'))),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
-                ),
-                color: Color.fromRGBO(220, 220, 20, 1),
-                borderRadius: BorderRadius.circular(20),
-              )),
-              FractionallySizedBox(
-                heightFactor: this.spendingPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(20),
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                  child: Text('${this.spendingAmount.toStringAsFixed(0)}'))),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 20, 1),
+                  borderRadius: BorderRadius.circular(20),
+                )),
+                FractionallySizedBox(
+                  heightFactor: this.spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text('${this.label}'),
-      ],
-    );
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(child: Text('${this.label}'))),
+        ],
+      );
+    });
   }
 }
