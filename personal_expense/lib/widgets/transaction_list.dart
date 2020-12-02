2d535af449a1c39ae9d8a2427a89d841aa0c82d0
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-02 18:02:43 -0500

    dynamically resizing heights

--- personal_expense/lib/widgets/transaction_list.dart
+++ personal_expense/lib/widgets/transaction_list.dart
@@ -11,59 +11,56 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: this.transactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  'No transaction added yet!',
-                  style: Theme.of(context).textTheme.title,
+    return this.transactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                'No transaction added yet!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: FittedBox(
-                            child: Text('\$${transactions[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      '${transactions[index].title}',
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: IconButton(
-                      color: Theme.of(context).errorColor,
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        deleteTx(transactions[index].id);
-                      },
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: FittedBox(
+                          child: Text('\$${transactions[index].amount}')),
                     ),
                   ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                  title: Text(
+                    '${transactions[index].title}',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                    color: Theme.of(context).errorColor,
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      deleteTx(transactions[index].id);
+                    },
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
