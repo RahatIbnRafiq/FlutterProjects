914ab0c38003037f49d5f70a87bdf26a11f22726
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-13 10:50:08 -0500

    restructure the lib folder

--- meals_app/lib/screens/category_meals_screen.dart
+++ meals_app/lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -12,12 +13,20 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
-    // final categoryId = routeArgs['id'];
+    final categoryId = routeArgs['id'];
+    final mealsOfThisCategory = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (ctx, index) {}),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(mealsOfThisCategory[index].title);
+        },
+        itemCount: mealsOfThisCategory.length,
+      ),
     );
   }
 }
