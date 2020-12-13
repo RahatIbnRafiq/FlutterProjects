ab0d97e568ef63fa7aad8e16bd03dcea182bf1da
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-13 10:23:40 -0500

    navigations and fonts

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/category_item.dart';
+import './dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => CategoryItem(
+                  categoryData.id,
+                  categoryData.title,
+                  categoryData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
