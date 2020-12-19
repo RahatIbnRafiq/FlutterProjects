9dd23eefd17bc72a8a57fc0a5a26d3ee4f2cc8b9
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-19 13:57:43 -0500

    add categories screen

--- meals_app/lib/screens/categories_screen.dart
+++ meals_app/lib/screens/categories_screen.dart
@@ -6,25 +6,20 @@ import '../data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeal'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map((categoryData) => CategoryItem(
-                  categoryData.id,
-                  categoryData.title,
-                  categoryData.color,
-                ))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map((categoryData) => CategoryItem(
+                categoryData.id,
+                categoryData.title,
+                categoryData.color,
+              ))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
