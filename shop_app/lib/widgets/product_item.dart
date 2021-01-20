576176345d003cccb174afebc1e263f0350b2507
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2021-01-20 13:28:49 -0500

    adding product item widget

---
+++ shop_app/lib/widgets/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(
+    this.id,
+    this.title,
+    this.imageUrl,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ), // placed at the start of the bar
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
